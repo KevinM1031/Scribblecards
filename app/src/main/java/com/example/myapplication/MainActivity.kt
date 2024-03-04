76218fae0c6e74b7910f13ddda1c971018851021
kevin103 <kevinmin1031@gmail.com>
2024-03-04 23:54:09 +0900

    backup

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -20,6 +28,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,7 +39,68 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                DiceRollerApp()
+                ArtSpaceApp(Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtSpaceApp(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = null,
+            modifier = Modifier.padding(0.dp, 8.dp)
+        )
+
+        Spacer(modifier.weight(1f))
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Artwork Title",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text (
+                text = "Artwork Artist (Year)",
+                fontSize = 10.sp
+            )
+        }
+
+        Spacer(modifier.weight(0.5f))
+
+        Row(
+            modifier = Modifier.padding(16.dp, 8.dp)
+        ) {
+            Button(
+                onClick = {  },
+                modifier = Modifier.size(80.dp, 20.dp)
+
+            ) {
+                Text (
+                    text = "Previous",
+                    fontSize = 8.sp
+                )
+            }
+            Spacer(modifier.weight(0.5f))
+            Button (
+                onClick = {  },
+                modifier = Modifier.size(160.dp, 40.dp)
+
+            ) {
+                Text (
+                    text = "Next",
+                    fontSize = 16.sp
+                )
             }
         }
     }
@@ -38,39 +108,8 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage()
-}
-
-@Composable
-fun DiceWithButtonAndImage(
-    modifier: Modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-) {
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when (result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
-
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = "1")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
-            Text(
-                text = stringResource(R.string.roll),
-                fontSize = 24.sp
-            )
-        }
+fun ArtSpaceAppPreview() {
+    MyApplicationTheme {
+        ArtSpaceApp()
     }
 }
