c6c24ad9d1ecc0debdfbfc1dc6c3f61900b03384
kevin103 <kevinmin1031@gmail.com>
2024-03-07 22:59:22 +0900

    backup

--- app/src/main/java/com/example/flashcards/MainActivity.kt
+++ app/src/main/java/com/example/flashcards/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.flashcards
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.flashcards.ui.theme.FlashcardsTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,21 +44,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Loading(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Loading(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Spacer(Modifier.weight(1f))
+        Image(
+            painter = painterResource(id = R.drawable.dice_1),
+            contentDescription = null
+        )
+        Spacer(Modifier.weight(0.3f))
+        Text(
+            text = stringResource(R.string.loading_title),
+            fontSize = 28.sp,
+            textAlign = TextAlign.Center
+        )
+        Spacer(Modifier.weight(2f))
+    }
 }
 
 @Composable
-fun MainMenu() {}
+fun MainMenu() {
+    Button(
+        onClick = {}
+    ) {
+        Text(
+            text = stringResource(R.string.main_menu_button_cards)
+        )
+    }
+
+    Button(
+        onClick = {}
+    ) {
+        Text(
+            text = stringResource(R.string.main_menu_button_cards)
+        )
+    }
+
+    Button(
+        onClick = {}
+    ) {
+        Text(
+            text = stringResource(R.string.main_menu_button_cards)
+        )
+    }
+}
 
 @Composable
 fun DecksMenu() {}
 
+//TEMP
 @Composable
-fun DeckView() {}
+fun DecksMenu_Open() {}
+
+@Composable
+fun DeckOverview() {}
+
+@Composable
+fun DeckContents() {}
+
+@Composable
+fun ImportCards() {}
+
+@Composable
+fun CreateCard() {}
 
 @Composable
 fun FlashcardSession() {}
@@ -56,6 +121,6 @@ fun SessionSummary() {}
 @Composable
 fun GreetingPreview() {
     FlashcardsTheme {
-        Greeting("Android")
+        Loading(modifier = Modifier.fillMaxSize())
     }
 }
